cbed0711e5a60359047f308bc748c42f6ebe2c28
Dima Lupyak <dima.lupyak@gmail.com>
2016-11-03 13:51:20 +0200

    Added building creation

---
+++ Assets/Scripts/WindowBuildingCreation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowBuildingCreation : Window
+{
+    public Button okButton;
+    public Button cancelButton;
+    public Building selectedBuilding;
+
+    public void SetSelectedBuilding(Building building)
+    {
+        selectedBuilding = building;
+        if (selectedBuilding != null)
+        {
+            base.Open();
+        }
+        else
+        {
+            base.Close();
+        }
+    }
+
+    void Start()
+    {
+        okButton.myAction = () =>
+        {
+            //TODO: BuildngCreation
+            GameObject.FindObjectOfType<MapController>().FinishBuilding();
+            SetSelectedBuilding(null);
+        };
+        cancelButton.myAction = () =>
+        {
+            SetSelectedBuilding(null);
+        };
+    }
+}
