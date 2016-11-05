8be48ede7deabd6b5dfddf670415b9cebe90d36e
Happaro <happaro@gmail.com>
2016-11-05 06:17:29 +0200

    sale all button

--- Assets/Scripts/WindowInventory.cs
+++ Assets/Scripts/WindowInventory.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class WindowInventory : Window 
 {
-	public Button closeButton;
+	public Button closeButton, saleAllButton;
 	public GameObject productPrefab;
 
 	public List<SaleButton> buttons = new List<SaleButton> ();
@@ -19,6 +19,29 @@ public class WindowInventory : Window
             base.Close(true);
 			WindowManager.Instance.GetWindow<GUI>().Open();
 		};
+
+		saleAllButton.myAction = () => 
+		{
+			int priceSum = 0;
+			int count = 0;
+			for (int i = 0; i < MainController.ins.inventory.productsCounts.Length; i++)
+			{
+				var saleCount = MainController.ins.inventory.productsCounts[i];
+
+				var price = BASE.Instance.GetResourcePrice((BuildingType)i);
+				MainController.ins.inventory.productsCounts[i] -= saleCount;
+				SaveManager.coinsCount += price * saleCount;
+				SaveManager.currentScore += price * saleCount;
+				priceSum += price * saleCount;
+				count += saleCount;
+			}
+			UpdateInventory();
+			Close(false);
+			WindowManager.Instance.GetWindow<WindowInfo>().Open("Успех!", string.Format("Вы продали \n{0} продуктов \nна суму {1}$", count, priceSum), () => {
+				WindowManager.Instance.GetWindow<WindowInfo>().Close(true);
+				WindowManager.Instance.GetWindow<GUI>().Open(true);
+			});
+		};
 	}
 
 	public override void Open()
@@ -29,6 +52,11 @@ public class WindowInventory : Window
 
 	void UpdateInventory()
 	{
+		saleAllButton.SetActive (false);
+		for (int i = 0; i < MainController.ins.inventory.productsCounts.Length; i++)
+			if (MainController.ins.inventory.productsCounts [i] > 0)
+				saleAllButton.SetActive (true);
+		
 		foreach (var button in buttons)
 			Destroy (button.gameObject);
 		buttons.Clear();
